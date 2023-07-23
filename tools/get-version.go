fefcc168ead42b035ae1b7eb2135158537f60223
Patrick Organ <patrick@permafrost.dev>
2023-07-23 01:42:47 -0400

    update tools

--- tools/get-version.go
+++ tools/get-version.go
@@ -1,43 +1,35 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/blang/semver"
 )
 
-const (
-	VERSION     = "0.0.0"
-	TARGET_FILE = "lib/version/app-version.go"
-)
+// func getTag(match ...string) (string, *semver.PRVersion) {
+// 	args := append([]string{
+// 		"describe", "--tags",
+// 	}, match...)
+// 	tag, err := exec.Command("git", args...).Output()
+// 	if err != nil {
+// 		return "", nil
+// 	}
+// 	tagParts := strings.Split(string(tag), "-")
+// 	if len(tagParts) == 3 {
+// 		if ahead, err := semver.NewPRVersion(tagParts[1]); err == nil {
+// 			return tagParts[0], &ahead
+// 		}
+// 	} else if len(tagParts) == 4 {
+// 		if ahead, err := semver.NewPRVersion(tagParts[2]); err == nil {
+// 			return tagParts[0] + "-" + tagParts[1], &ahead
+// 		}
+// 	}
 
-func getTag(match ...string) (string, *semver.PRVersion) {
-	args := append([]string{
-		"describe", "--tags",
-	}, match...)
-	tag, err := exec.Command("git", args...).Output()
-	if err != nil {
-		return "", nil
-	}
-	tagParts := strings.Split(string(tag), "-")
-	if len(tagParts) == 3 {
-		if ahead, err := semver.NewPRVersion(tagParts[1]); err == nil {
-			return tagParts[0], &ahead
-		}
-	} else if len(tagParts) == 4 {
-		if ahead, err := semver.NewPRVersion(tagParts[2]); err == nil {
-			return tagParts[0] + "-" + tagParts[1], &ahead
-		}
-	}
+// 	return string(tag), nil
+// }
 
-	return string(tag), nil
-}
-
-func getProjectVersion() string {
+func GetProjectVersion() string {
 	if tags, err := exec.Command("git", "tag").Output(); err != nil || len(tags) == 0 {
 		// no tags found -- fetch them
 		exec.Command("git", "fetch", "--tags").Run()
@@ -77,33 +69,4 @@ func getProjectVersion() string {
 	}
 
 	return version.String()
-}
-
-func RunGitCommand(args ...string) string {
-	cmd := exec.Command("git", args...)
-
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil {
-		return ""
-	}
-
-	return strings.TrimSpace(out.String())
-}
-
-func main() {
-	result := getProjectVersion() //RunGitCommand("rev-parse", "HEAD")[0:8]
-
-	file, err := os.Create(TARGET_FILE)
-	if err != nil {
-		fmt.Printf("Cannot create %s file: %v\n", TARGET_FILE, err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	file.WriteString(fmt.Sprintf("package version\n\nconst APP_VERSION = \"%s\"\n", result))
 }
